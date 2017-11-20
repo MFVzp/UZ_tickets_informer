c06ca37b0e0f0e07e31eb1c08f67e119769fadcd
Ilya <tauruszpua@yahoo.com>
2017-11-20 14:04:01 +0200

    add good output

--- UkrZaliz/uz_interface.py
+++ UkrZaliz/uz_interface.py
@@ -14,7 +14,6 @@ if __name__ == '__main__':
 
     direct = Direction(args.station_from, args.station_till, args.date_dep, args.coach_type)
     all_coaches = direct.get_info()
-    print(all_coaches, '\n')
     GOOD_COACHES = list(range(5, 32, 2))
     for train in all_coaches:
         carriages = train.get('carriages')
@@ -28,5 +27,17 @@ if __name__ == '__main__':
                 carriage['coaches'] = good_coaches
             else:
                 carriage.clear()
+        train['carriages'] = [carriage for carriage in carriages if carriage]
+    all_coaches = [train for train in all_coaches if train.get('carriages')]
 
-    print(all_coaches)
+    if all_coaches:
+        for train in all_coaches:
+            print('Поезд: {}'.format(train.get('train')))
+            for carriage in train.get('carriages'):
+                print('\tВагон №{}, места: {}'.format(
+                    carriage.get('number'),
+                    ', '.join(carriage.get('coaches'))
+                ))
+            print('\n')
+    else:
+        print('Нет поездов с хорошими местами. Попробуйте другою дату.')
