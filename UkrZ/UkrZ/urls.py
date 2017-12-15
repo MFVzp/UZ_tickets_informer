bde0174f15881db24a88871865339dfeed003f47
MFVzp <tauruszpua@yahoo.com>
2017-12-15 10:37:40 +0200

    add namespace in urls

--- UkrZ/UkrZ/urls.py
+++ UkrZ/UkrZ/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('observe/', include('search_app.urls'))
+    path('observe/', include('search_app.urls', namespace='search'))
 
 ]
