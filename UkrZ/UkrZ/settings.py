c0c2eb99a4fa64cff6ef06ad9b2fcdaf348b7800
MFVzp <tauruszpua@yahoo.com>
2017-12-26 17:18:36 +0200

    add static

--- UkrZ/UkrZ/settings.py
+++ UkrZ/UkrZ/settings.py
@@ -128,6 +128,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 
 # UkrZ settings
 UZ_HOST = 'https://booking.uz.gov.ua/ru/'
--- UkrZ/UkrZ/urls.py
+++ UkrZ/UkrZ/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('auth_app.urls', namespace='auth')),
     path('observe/', include('search_app.urls', namespace='search')),
-
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
